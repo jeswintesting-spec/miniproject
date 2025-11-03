982b08ce3071bec1c357c021cbee18d5c332b15a
jeswintesting-spec <jeswintesting@gmail.com>
2025-11-03 15:02:59 +0530

    Delete chillspot directory

--- chillspot/admin/testdb.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "chillspot");
-
-if ($conn->connect_error) {
-  die("DB Connection failed: " . $conn->connect_error);
-}
-echo "Database Connected Successfully!";
-?>
